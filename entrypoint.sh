8a639aab8cf53c37bb2f10c2665f6abcfa30ea64
Sambasiva Battagiri <52483425+sbattagi@users.noreply.github.com>
2020-03-18 15:56:06 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,6 +31,7 @@ git remote add deploy $remote_repo && \
 git checkout $remote_branch || git checkout --orphan $remote_branch && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+echo "GITHUB_ACTOR=$GITHUB_ACTOR" && \
 git add . && \
 echo -n 'Files to Commit:' && ls -l | wc -l && \
 timestamp=$(date +%s%3N) && \
