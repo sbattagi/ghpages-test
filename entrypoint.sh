4bb01aa1767e1f7f6ecf0e0dca26a64e2b5f41f3
Sambasiva Battagiri <52483425+sbattagi@users.noreply.github.com>
2020-03-18 17:15:52 +0530

    fixed the issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,13 @@ ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts && \
 echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
 chmod 400 /root/.ssh/id_rsa
 echo '=================== Update all submodules ==================='
-cd docs-source
 git submodule init
 git submodule update --recursive --remote
+cd docs-source
 echo '=================== Build site ==================='
 HUGO_ENV=production hugo -v --minify -d docs
 echo '=================== Publish to GitHub Pages ==================='
-
+cd -
 remote_repo="https://github.com/${GITHUB_DEPLOY_REPOSITORY}.git" && \
 remote_branch=${GITHUB_DEPLOY_BRANCH} && \
 echo "samba:GITHUB_REPOSITORY =$GITHUB_REPOSITORY" && \
@@ -40,11 +40,8 @@ cd docs && \
 git add . && \
 echo -n 'Files to Commit:' && ls -l | wc -l && \
 timestamp=$(date +%s%3N) && \
-
 git commit -m "Automated deployment to GitHub Pages on $timestamp" > /dev/null 2>&1 && \
 git push deploy $remote_branch --force && \
-
 rm -fr .git && \
 cd ../
 echo '=================== Done  ==================='
-
